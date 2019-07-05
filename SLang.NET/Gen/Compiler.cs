71415b611b6d18e06d80f269b18efd6e39387ad6
ivan tkachenko <ratijas@users.noreply.github.com>
2019-07-05 12:18:52 +0300

    compile all global routine definitions + test

--- SLang.NET/Gen/Compiler.cs
+++ SLang.NET/Gen/Compiler.cs
@@ -7,7 +7,7 @@ namespace SLang.NET.Gen
 {
     public static class Compiler
     {
-        public static AssemblyDefinition CompileToIL(Compilation c, string name)
+        public static AssemblyDefinition CompileToIL(Compilation root, string name)
         {
             var nameDef = new AssemblyNameDefinition(name, new Version(1, 0, 0, 0));
             var asm = AssemblyDefinition.CreateAssembly(nameDef, "SLangMainModule", ModuleKind.Console);
@@ -17,8 +17,23 @@ namespace SLang.NET.Gen
             var globalAnonymousUnit =
                 context.GlobalUnit = new SLangUnitDefinition(context, new Identifier("SLang$GlobalUnit"));
 
-            var globalAnonymousRoutine = new SLangRoutineDefinition(globalAnonymousUnit, c.Anonymous);
+            var globalAnonymousRoutine = new SLangRoutineDefinition(globalAnonymousUnit, root.Anonymous);
             
+            foreach (var declaration in root.Declarations)
+            {
+                switch (declaration)
+                {
+                    case Routine routine:
+                    {
+                        var _ = new SLangRoutineDefinition(globalAnonymousUnit, routine);
+                        break;
+                    }
+
+                    default:
+                        throw new NotImplementedException("only routine declarations are supported");
+                }
+            }
+
             context.Compile();
 
             asm.EntryPoint = globalAnonymousRoutine.NativeMethod;
